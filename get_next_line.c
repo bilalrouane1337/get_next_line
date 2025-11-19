d1db779d91e942045fd4db4b0393f689cde5d1e2
bilalrouane1337 <bilal.rouane1337@gmail.com>
2025-11-19 23:28:19 +0100

    c1r3p7vm

--- get_next_line.c
+++ get_next_line.c
@@ -6,18 +6,17 @@
 /*   By: brouane <brouane@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/19 17:47:30 by brouane           #+#    #+#             */
-/*   Updated: 2025/11/19 18:20:48 by brouane          ###   ########.fr       */
+/*   Updated: 2025/11/19 23:24:48 by brouane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *fill_line(char *working_line, int fd)
+char *ft_fill_line(char *working_line, int fd)
 {
     size_t bytes;
-
+    
     bytes = read(fd, working_line, BUFFER_SIZE);
-
     if (bytes == -1)
         return (NULL);
     return (working_line);
@@ -43,6 +42,25 @@ char *ft_get_next_line(char *working_line)
     return (next_line);
 }
 
+char *ft_get_crop_line(char *working_line)
+{
+    char* crop_line;
+    int i;
+    int j;
+
+    i = 0;
+    while (working_line[i] && working_line[i] != '\n')
+        i++;
+    if(!working_line[i])
+        return (NULL);
+    crop_line = (char*)malloc(ft_strlen(working_line) - i);
+    j = 0;
+    while (working_line[++i])
+        crop_line[j++] = working_line[i];
+    crop_line[i] = 0;
+    return (crop_line);
+}
+
 char *get_next_line(int fd)
 {
     static char* working_line;
@@ -50,23 +68,39 @@ char *get_next_line(int fd)
     
     if (fd < 0 || BUFFER_SIZE <= 0)
         return (NULL);
-    working_line = (char*)malloc(BUFFER_SIZE + 1);
+    if(!working_line)
+        working_line = (char*)malloc(BUFFER_SIZE + 1);
     if (!working_line)
         return (NULL);
-    working_line = fill_line(working_line, fd);
+    working_line = ft_fill_line(working_line, fd);
     next_line = ft_get_next_line(working_line);
-    printf("%s\n", working_line);
+    working_line = ft_get_crop_line(working_line);
+    // printf("%s\n", working_line);
+    // printf("ffff\n");
+    return(next_line);
 }
 
 int main()
 {
     int fd = open("text.txt", O_RDONLY);
-    
-    char *holder;
-    
-    while (((holder = get_next_line(fd)) != NULL))
-    {
-        printf("%s\n", holder);
-    }
+
+    char *holder = get_next_line(fd);
+    printf("%s\n", holder);
+
+    holder = get_next_line(fd);
+    printf("%s\n", holder);
+
+    holder = get_next_line(fd);
+    printf("%s\n", holder);
+
+    holder = get_next_line(fd);
+    printf("%s\n", holder);
+  
+    // char *holder;
+    // while ((holder = get_next_line(fd)) != NULL)
+    // {
+    //     printf("%s\n", holder);
+    // }
+
     close(fd);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,30 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: brouane <brouane@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/19 17:48:59 by brouane           #+#    #+#             */
-/*   Updated: 2025/11/19 21:47:23 by brouane          ###   ########.fr       */
+/*   Created: 2025/11/19 20:47:34 by brouane           #+#    #+#             */
+/*   Updated: 2025/11/19 20:47:57 by brouane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+#include "get_next_line.h"
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
 
-
-
-#include <stdio.h>
-
-
-
-#define BUFFER_SIZE 17
-
-size_t	ft_strlen(const char *s);
-
-#endif
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
